613cad3d9b060d8efa158787bd01bdf950856d7c
Pavlo Hodysh <gaarasmail@gmail.com>
2015-10-15 13:34:45 +0300

    implemented sorting algorithms but not properly working

---
+++ Discnt.Tests/DiscntTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Discnt.Tests
+{
+    [TestClass]
+    public class DiscntTests
+    {
+        private const string Message = "Arrays mismatch!";
+
+        private int[] GetShuffledArray()
+        {
+            return new[]
+            {
+                389, 100, 331, 340, 325, 331, 462,
+                307, 457, 86, 398, 232, 97, 449, 14,
+                236, 491, 126, 339, 187, 64, 142, 498,
+                433, 437, 343, 401, 199, 138, 167,
+                21, 306, 75, 105, 392, 430, 73, 271,
+                479, 201, 252, 2, 101, 219, 472, 146,
+                21, 40, 444, 436, 202, 479, 104, 227,
+                130, 246, 102, 397, 403, 481, 429,
+                263, 168, 290, 65, 91, 157, 491, 57,
+                84, 162, 220, 147, 138, 435, 62, 41,
+                278, 461, 290, 321, 361, 255, 202,
+                464, 41, 201, 347, 475, 145, 327, 175,
+                367, 497, 271, 489, 333, 311, 153, 380
+            };
+        }
+
+        private int[] GetSortedArray()
+        {
+            return new[]
+            {
+                2, 14, 21, 21, 40, 41, 41, 57, 62, 64,
+                65, 73, 75, 84, 86, 91, 97, 100, 101,
+                102, 104, 105, 126, 130, 138, 138, 142,
+                145, 146, 147, 153, 157, 162, 167, 168,
+                175, 187, 199, 201, 201, 202, 202, 219,
+                220, 227, 232, 236, 246, 252, 255, 263,
+                271, 271, 278, 290, 290, 306, 307, 311,
+                321, 325, 327, 331, 331, 333, 339, 340,
+                343, 347, 361, 367, 380, 389, 392, 397,
+                398, 401, 403, 429, 430, 433, 435, 436,
+                437, 444, 449, 457, 461, 462, 464, 472,
+                475, 479, 479, 481, 489, 491, 491, 497, 498
+            };
+        }
+
+        [TestMethod]
+        public void SelectionSort_ShouldSort()
+        {
+            // Assign
+            var discnt = new discnt.Discnt();
+            var shuffled = GetShuffledArray();
+            var sorted = GetSortedArray();
+
+            // Act
+            discnt.SelectionSort(shuffled);
+
+            // Assert
+            Assert.IsTrue(shuffled.SequenceEqual(sorted), Message);
+            CollectionAssert.AreEquivalent(shuffled, sorted, Message);
+        }
+
+        [TestMethod]
+        public void InsertionSort_ShouldSort()
+        {
+            // Assign
+            var discnt = new discnt.Discnt();
+            var shuffled = GetShuffledArray();
+            var sorted = GetSortedArray();
+
+            // Act
+            discnt.InsertionSort(shuffled);
+
+            // Assert
+            Assert.IsTrue(shuffled.SequenceEqual(sorted), Message);
+            CollectionAssert.AreEquivalent(shuffled, sorted, Message);
+        }
+
+        [TestMethod]
+        public void BubbleSort_ShouldSort()
+        {
+            // Assign
+            var discnt = new discnt.Discnt();
+            var shuffled = GetShuffledArray();
+            var sorted = GetSortedArray();
+
+            // Act
+            discnt.BubbleSort(shuffled);
+
+            // Assert
+            Assert.IsTrue(shuffled.SequenceEqual(sorted), Message);
+            CollectionAssert.AreEquivalent(shuffled, sorted, Message);
+        }
+    }
+}
--- discnt/discnt.cs
+++ discnt/discnt.cs
@@ -1,10 +1,121 @@
-﻿namespace discnt
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace discnt
 {
     internal class Discnt
     {
         private static void Main(string[] args)
         {
+            var discnt = new Discnt();
+            string inputFileName;
+            string outputFileName;
 
+            if (args == null || args.Length == 0)
+            {
+                inputFileName = "discnt.in";
+                outputFileName = "discnt.in";
+            }
+            else if (args.Length == 2)
+            {
+                inputFileName = args[0];
+                outputFileName = args[1];
+            }
+            else
+            {
+                Console.WriteLine("Command line parameters violation!");
+                return;
+            }
+
+            discnt.Run(inputFileName, outputFileName);
+        }
+
+        internal void Run(string inputFileName, string outputFileName)
+        {
+            var lines = File.ReadLines(inputFileName).ToArray();
+            var prices = lines[0].Split(' ').Select(int.Parse).ToArray();
+            int discount = int.Parse(lines[1]);
+
+            // input data is 10000 = 100^2 complexity at max
+            // which is the comlexity of the algorithm O(N^2)
+            // so suggestion is to use this one for less than 100 elements
+            // to not overhead the top complexity overall
+            if (prices.Length < 100)
+            {
+                SelectionSort(prices);
+            }
+            else
+            {
+                InsertionSort(prices);
+            }
+
+            double result = Math.Round(2.1467, 2, MidpointRounding.ToEven);
+            File.WriteAllText(outputFileName, result.ToString(CultureInfo.InvariantCulture));
+        }
+
+        internal void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minIdex = i;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (IsGreater(array[i], array[j]))
+                    {
+                        minIdex = j;
+                    }
+                }
+
+                Swap(array, i, minIdex);
+            }
+        }
+
+        internal void InsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (IsGreater(array[j], array[j - 1]))
+                    {
+                        Swap(array, j, j - 1);
+                    }
+                }
+            }
+        }
+
+        internal void BubbleSort(int[] array)
+        {
+            bool somethingSwapped = true;
+
+            while (somethingSwapped)
+            {
+                somethingSwapped = false;
+
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (IsGreater(array[i], array[i + 1]))
+                    {
+                        Swap(array, i, i + 1);
+                        somethingSwapped = true;
+                    }
+                }
+            }
+        }
+
+        internal bool IsGreater(int left, int right)
+        {
+            return left > right;
+        }
+
+        internal void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
         }
     }
 }
