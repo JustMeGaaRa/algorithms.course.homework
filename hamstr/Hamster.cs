ccfe328f7bf007154125e987f56bead43f684015
Pavlo Hodysh <gaarasmail@gmail.com>
2015-11-04 17:41:25 +0200

    replaced old files with new ones

---
+++ hamstr/Hamster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace hamstr
+{
+    public class Hamster : IComparable<Hamster>
+    {
+        public Hamster(int portion, int greed)
+        {
+            Portion = portion;
+            Greed = greed;
+        }
+
+        public long Portion { get; }
+
+        public long Greed { get; }
+
+        public HamsterCage Cage { get; set; }
+
+        public long Consumes
+        {
+            get { return Portion + Greed * (Cage.HamstersCount - 1); }
+        }
+
+        public int CompareTo(Hamster other)
+        {
+            if (Consumes < other.Consumes)
+                return -1;
+
+            if (Consumes > other.Consumes)
+                return 1;
+
+            return 0;
+        }
+    }
+}
---
+++ hamstr/hamstr.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace hamstr
+{
+    public class Hamstr
+    {
+        private static void Main(string[] args)
+        {
+            var hamstr = new Hamstr();
+            string inputFileName;
+            string outputFileName;
+
+            if (args == null || args.Length == 0)
+            {
+                inputFileName = "hamstr.in";
+                outputFileName = "hamstr.out";
+            }
+            else if (args.Length == 2)
+            {
+                inputFileName = args[0];
+                outputFileName = args[1];
+            }
+            else
+            {
+                Console.WriteLine("Command line parameters violation!");
+                return;
+            }
+
+            hamstr.Run(inputFileName, outputFileName);
+        }
+
+        public void Run(string inputFileName, string outputFileName)
+        {
+            try
+            {
+                var lines = File.ReadLines(inputFileName).ToArray();
+
+                // 0 <= S <= 1 000 000 000
+                int foodSupplies = int.Parse(lines[0]);
+
+                // 1 <= C <= 100 000
+                int hamsterCount = int.Parse(lines[1]);
+
+                var cage = new HamsterCage(foodSupplies, hamsterCount);
+
+                var hamsters = lines.Skip(2).Select(line =>
+                {
+                    var numbers = line.Split(' ');
+                    int h = int.Parse(numbers[0]);
+                    int g = int.Parse(numbers[1]);
+                    return new Hamster(h, g);
+                });
+
+                var result = cage.FeedHamsters(hamsters);
+                File.WriteAllText(outputFileName, result.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
