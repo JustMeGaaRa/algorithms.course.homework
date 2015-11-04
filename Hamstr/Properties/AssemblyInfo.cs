f96eb141c89c1b9b2efc5b9b7911de0f05c74076
Pavlo Hodysh <gaarasmail@gmail.com>
2015-11-04 12:30:15 +0200

    fixed naming for testing system

--- Hamstr/Properties/AssemblyInfo.cs
+++ Hamstr/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hamstr")]
+[assembly: AssemblyTitle("hamstr")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Hamstr")]
+[assembly: AssemblyProduct("hamstr")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
