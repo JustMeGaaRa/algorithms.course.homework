935169ffeb06707793ed1ad68bb3fbf833758eb8
Pavlo Hodysh <gaarasmail@gmail.com>
2015-11-23 22:53:48 +0200

    implemented bugtrk task

---
+++ BugtrkTests/BugtrkTests.cs
@@ -0,0 +1,213 @@
+﻿using bugtrk;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BugtrkTests
+{
+    [TestClass]
+    public class BugtrkTests
+    {
+        [TestMethod]
+        public void FindMinLength_Case1()
+        {
+            // Assign
+            long count = 10;
+            int width = 2;
+            int height = 3;
+            var expected = 9;
+            var bugtrk = new Bugtrk();
+
+            // Act
+            var result = bugtrk.FindMinLength(count, width, height);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void FindMinLength_Case2()
+        {
+            // Assign
+            long count = 2;
+            int width = 1000000000;
+            int height = 999999999;
+            var expected = 1999999998;
+            var bugtrk = new Bugtrk();
+
+            // Act
+            var result = bugtrk.FindMinLength(count, width, height);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void FindMinLength_Case3()
+        {
+            // Assign
+            long count = 4;
+            int width = 1;
+            int height = 1;
+            var expected = 2;
+            var bugtrk = new Bugtrk();
+
+            // Act
+            var result = bugtrk.FindMinLength(count, width, height);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void FindMinLength_Case4()
+        {
+            // Assign
+            long count = 32416188257;
+            int width = 105143;
+            int height = 15489949;
+            var expected = 229777903466;
+            var bugtrk = new Bugtrk();
+
+            // Act
+            var result = bugtrk.FindMinLength(count, width, height);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void FindMinLength_Case5()
+        {
+            // Assign
+            long count = 100;
+            int width = 1000000000;
+            int height = 1;
+            var expected = 1000000000;
+            var bugtrk = new Bugtrk();
+
+            // Act
+            var result = bugtrk.FindMinLength(count, width, height);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void FindMinLength_Case6()
+        {
+            // Assign
+            long count = 1;
+            int width = 1;
+            int height = 999999999;
+            var expected = 999999999;
+            var bugtrk = new Bugtrk();
+
+            // Act
+            var result = bugtrk.FindMinLength(count, width, height);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void FindMinLength_Case7()
+        {
+            // Assign
+            long count = 1000000000000;
+            int width = 1000000000;
+            int height = 1000000000;
+            var expected = 1000000000000000;
+            var bugtrk = new Bugtrk();
+
+            // Act
+            var result = bugtrk.FindMinLength(count, width, height);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void FindMinLength_Case8()
+        {
+            // Assign
+            long count = 1000000000000;
+            int width = 1;
+            int height = 1;
+            var expected = 1000000;
+            var bugtrk = new Bugtrk();
+
+            // Act
+            var result = bugtrk.FindMinLength(count, width, height);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void FindMinLength_Case9()
+        {
+            // Assign
+            long count = 1;
+            int width = 1;
+            int height = 1;
+            var expected = 1;
+            var bugtrk = new Bugtrk();
+
+            // Act
+            var result = bugtrk.FindMinLength(count, width, height);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void FindMinLength_Case10()
+        {
+            // Assign
+            long count = 626;
+            int width = 1;
+            int height = 1;
+            var expected = 26;
+            var bugtrk = new Bugtrk();
+
+            // Act
+            var result = bugtrk.FindMinLength(count, width, height);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void FindMinLength_Case11()
+        {
+            // Assign
+            long count = 1000000000000;
+            int width = 999;
+            int height = 999;
+            var expected = 999000000;
+            var bugtrk = new Bugtrk();
+
+            // Act
+            var result = bugtrk.FindMinLength(count, width, height);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void FindMinLength_Case12()
+        {
+            // Assign
+            long count = 998001;
+            int width = 1000000000;
+            int height = 1000000000;
+            var expected = 999000000000;
+            var bugtrk = new Bugtrk();
+
+            // Act
+            var result = bugtrk.FindMinLength(count, width, height);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
--- bugtrk/bugtrk.cs
+++ bugtrk/bugtrk.cs
@@ -1,15 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace bugtrk
 {
-    class Bugtrk
+    public class Bugtrk
     {
         static void Main(string[] args)
         {
+            var bugtrk = new Bugtrk();
+            string inputFileName;
+            string outputFileName;
+
+            if (args == null || args.Length == 0)
+            {
+                inputFileName = "bugtrk.in";
+                outputFileName = "bugtrk.out";
+            }
+            else if (args.Length == 2)
+            {
+                inputFileName = args[0];
+                outputFileName = args[1];
+            }
+            else
+            {
+                Console.WriteLine("Command line parameters violation!");
+                return;
+            }
+
+            bugtrk.Run(inputFileName, outputFileName);
+        }
+
+        public void Run(string inputFileName, string outputFileName)
+        {
+            var line = File.ReadAllText(inputFileName);
+            var numbers = line.Split(' ').ToArray();
+
+            var count = long.Parse(numbers[0]);
+            var width = int.Parse(numbers[1]);
+            var height = int.Parse(numbers[2]);
+
+            var result = FindMinLength(count, width, height);
+            File.WriteAllText(outputFileName, result.ToString());
+        }
+
+        public long FindMinLength(long count, int width, int height)
+        {
+            if (count == 0)
+                return 0;
+
+            if (count == 1)
+                return Math.Max(width, height);
+
+            if (count == 2)
+                return Math.Min(width * count, height * count);
+
+            long begin = 0;
+            long end = count;
+            long minimumLength = GetMinLength(begin, end, count, width, height);
+
+            while (begin <= end)
+            {
+                var middle = (begin + end) / 2;
+
+                var leftMin = GetMinLength(begin, middle, count, width, height);
+                var rightMin = GetMinLength(middle + 1, end, count, width, height);
+
+                // if splitting into rows is starting to get bigger
+                //if (leftMin >= minimumLength && rightMin >= minimumLength)
+                //{
+                //    break;
+                //}
+
+                // else use the minimum length found
+                var currentMinimum = Math.Min(leftMin, rightMin);
+                if (currentMinimum < minimumLength)
+                {
+                    minimumLength = currentMinimum;
+                }
+
+                // and continue on the lesser side
+                if (leftMin < rightMin)
+                {
+                    end = middle;
+                }
+                else
+                {
+                    begin = middle + 1;
+                }
+            }
+
+            return minimumLength;
+        }
+
+        private long GetMinLength(long begin, long end, long count, int width, int height)
+        {
+            var middle = (begin + end) / 2;
+            var elementsPerLine = middle;
+
+            if (elementsPerLine == 0)
+                elementsPerLine = count;
+
+            var lines = count / elementsPerLine;
+            if (count % elementsPerLine != 0)
+            {
+                lines++;
+            }
+
+            var totalWidth = elementsPerLine * width;
+            var totalHeight = height * lines;
+            return Math.Max(totalWidth, totalHeight);
         }
     }
 }
