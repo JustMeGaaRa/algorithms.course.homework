b08308f9b5fc39b57b0f3b5db61cbc06ef86c274
Pavlo Hodysh <gaarasmail@gmail.com>
2015-12-29 18:24:59 +0200

    added wchain stub project

---
+++ WchainTests/WchainTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using wchain;
+
+namespace WchainTests
+{
+    [TestClass]
+    public class WchainTests
+    {
+        [TestMethod]
+        public void FindMaximumChainLength_Case1()
+        {
+            // Assign
+            var wchain = new Wchain();
+            var lines = new[]
+                            {
+                                "10", "crates", "car", "cats", "crate",
+                                "rate", "at", "ate", "tea", "rat", "a"
+                            };
+            var expected = 6;
+
+            // Act
+            var actual = wchain.FindMaximumChainLength(lines);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindMaximumChainLength_Case2()
+        {
+            // Assign
+            var wchain = new Wchain();
+            var lines = new[] { "5", "b", "bcad", "bca", "bad", "bd" };
+            var expected = 4;
+
+            // Act
+            var actual = wchain.FindMaximumChainLength(lines);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindMaximumChainLength_Case3()
+        {
+            // Assign
+            var wchain = new Wchain();
+            var lines = new[] { "3", "word", "anotherword", "yetanotherword" };
+            var expected = 1;
+
+            // Act
+            var actual = wchain.FindMaximumChainLength(lines);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindMaximumChainLength_Case4()
+        {
+            // Assign
+            var wchain = new Wchain();
+            var lines = new[] { "6", "a", "b", "ba", "bca", "bda", "bdca" };
+            var expected = 4;
+
+            // Act
+            var actual = wchain.FindMaximumChainLength(lines);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindMaximumChainLength_Case5()
+        {
+            // Assign
+            var wchain = new Wchain();
+            var lines = new[] { "7", "e", "bca", "f", "ba", "bda", "bdefgh", "bdca" };
+            var expected = 3;
+
+            // Act
+            var actual = wchain.FindMaximumChainLength(lines);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindMaximumChainLength_Case6()
+        {
+            // Assign
+            var wchain = new Wchain();
+            var lines = new[] { "3", "a", "b", "c" };
+            var expected = 1;
+
+            // Act
+            var actual = wchain.FindMaximumChainLength(lines);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindMaximumChainLength_Case7()
+        {
+            // Assign
+            var wchain = new Wchain();
+            var lines = new[] { "7", "a", "b", "c", "d", "e", "dea", "de" };
+            var expected = 3;
+
+            // Act
+            var actual = wchain.FindMaximumChainLength(lines);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindMaximumChainLength_Case8()
+        {
+            // Assign
+            var wchain = new Wchain();
+            var lines = new string[51];
+            lines[0] = "50";
+
+            for (int i = 1; i < 51; i++)
+            {
+                lines[i] = new string('a', i);
+            }
+
+            var expected = 50;
+
+            // Act
+            var actual = wchain.FindMaximumChainLength(lines);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ wchain/wchain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace wchain
+{
+    public class Wchain
+    {
+        private static void Main(string[] args)
+        {
+            var wchain = new Wchain();
+            string inputFileName;
+            string outputFileName;
+
+            if (args == null || args.Length == 0)
+            {
+                inputFileName = "wchain.in";
+                outputFileName = "wchain.out";
+            }
+            else if (args.Length == 2)
+            {
+                inputFileName = args[0];
+                outputFileName = args[1];
+            }
+            else
+            {
+                Console.WriteLine("Command line parameters violation!");
+                return;
+            }
+
+            wchain.Run(inputFileName, outputFileName);
+        }
+
+        public void Run(string inputFileName, string outputFileName)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+            var result = FindMaximumChainLength(lines);
+            File.WriteAllText(outputFileName, result.ToString());
+        }
+
+        public int FindMaximumChainLength(string[] lines)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
